7d5fdbd043b6467723253dc12a4236322e40044a
Aicha AL-Rob <aicha.k2002@gmail.com>
2024-04-28 17:00:40 -0300

    resolvendo problemas

--- ByteBites/admin.py
+++ ByteBites/admin.py
@@ -1,15 +1,13 @@
 from django.contrib import admin
-
+from .models import Recipe, Ingredient, RecipeIngredient, User, Likes
 # Register your models here.
 # use admin interface to add edit and delete recipes, only admin can access this
 
-# from .models import Recipe, Ingredient, RecipeIngredient, User, Likes, Save
-
-# admin.site.register(Recipe)
-# admin.site.register(Ingredient)
-# admin.site.register(RecipeIngredient)
-# admin.site.register(User)
-# admin.site.register(Likes)
+admin.site.register(Recipe)
+admin.site.register(Ingredient)
+admin.site.register(RecipeIngredient)
+admin.site.register(User)
+admin.site.register(Likes)
 
 # Save model is not registered as it is not required to be accessed by admin
 
